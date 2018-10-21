9cf94f094632a714993c6ee55493323d8b981468
Gurlabe Dhaliwal <GurlabeD@Ramans-MacBook-Air.local>
2018-10-20 20:45:14 -0700

    JS fiddle was not cooperating. added photo of completed

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var elements = [
-      { name: 'Hydrogen', number: 1, symbol: 'H' },
-      { name: 'Mercury', number: 80, symbol: 'Hg' },
-      { name: 'Lead', number: 82, symbol: 'Pb' },
-      { name: 'Bismuth', number: 83, symbol: 'Bi' },
-      { name: 'Neon', number: 10, symbol: 'Ne' }
-    ]
-  })
+var elementsArray = [
+  {name: 'Hydrogen', number: '1', symbol: 'H'}, 
+  {name: 'Mercury', number: '80', symbol: 'Hg'},
+  {name: 'Lead', number: '82', symbol: 'Pb'},
+  {name: 'Bismuth', number: '83', symbol: 'Bi'},
+  {name: 'Neon', number: '10', symbol: 'Hg'},
+  ];
+  
+  function displayTable(periodicTable){   
+      var table = document.getElementById('periodicTable');
+      for (var i = 0; i < periodicTable.length; ++i) {   
+          var attribute = periodicTable[i];
+          var row = document.createElement('tr');       
+          var properties = ['name', 'number', 'symbol'];
+  
+          for (var j = 0; j < properties.length; ++j) {  
+              var cell = document.createElement('td');
+              cell.innerHTML = attribute[properties[j]];
+              row.appendChild(cell);
+          } 
+          table.appendChild(row);
+      }
+  }
+  displayTable(elementsArray);
